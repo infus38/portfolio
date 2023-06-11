584cb1e6c07311c4a4709f0327ea43ab775df5b9
infus38 <infus38@gmail.com>
2023-06-11 20:35:26 +0200

    index.html & script.js: add the ability to download my resume
    Signed-off-by: infus38 <infus38@gmail.com>

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const stringDictionary = {
         </p>
         <svg style="bottom: 0;" width="9" height="9" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z" fill="currentColor"></path></svg>
         <a href="mailto:infus38@gmail.com"> Contact Me</a>
+        <svg style="bottom: 0; margin-left: 1%" width="9" height="9" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z" fill="currentColor"></path></svg>
+        <a href="./resources/cv.pdf" download>My Resume</a>
     `,
     btn1: `
         <h1>Experience and Expertise</h1>
